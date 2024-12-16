68e2e97d5840486bc71a581dd2f56e1c03f53cfa
sweet-fox <burikovsvatoslav@gmail.com>
2024-12-16 21:09:10 +0000

    rewrite all node add, fixed count_symb create and count_map

--- main.cpp
+++ main.cpp
@@ -6,15 +6,13 @@
 #include <algorithm>
 
 using namespace std;
-//you need to write map to vector in this open
 
-
-struct Node {
+struct node {
 	int count;
-	Node* left_1;
-	Node* right_0;
+	node* left_1;
+	node* right_0;
 
-	Node(int value){
+	node(int value){
 		count = value;
 		left_1=nullptr;
 		right_0=nullptr;
@@ -29,9 +27,9 @@ public:
 	string main_line;
 	
 	vector<pair<char,int>> count_map;
-	vector<Node> count_symb;
+	vector<node> count_symb;
 
-	static bool compareByValue(const pair<char,int>& a, const pair<char,int>& b){
+	static bool compare_value_map(const pair<char,int>& a, const pair<char,int>& b){
 		return a.second < b.second;
 	}
 
@@ -56,11 +54,13 @@ public:
 		cout << main_line << endl;
 	}
 	
-	//in proces(writing vector tipe)
 	void count_symbols_return_map(){
 		int pos = 0;
 		for (char i : main_line){
-			if (count_map.find(i) == count_map.end()){
+			auto iter = find_if(count_map.begin(), count_map.end(), [&i](const pair<char,int>& pair){
+						return pair.first == i;
+					});
+			if (iter == count_map.end()){
 				count_map.push_back(make_pair(i,1));
 			}
 			else{
@@ -74,7 +74,7 @@ public:
 			}
 
 		}
-		sort(count_map.begin(),count_map.end(),compareByValue);
+		sort(count_map.begin(),count_map.end(),compare_value_map);
 	}
 	
 	void print_map_count(){
@@ -91,12 +91,30 @@ public:
 	
 	}
 	
-	void print_sort_Node(){
+	void print_sort_node(){
 		for(const auto& n: count_symb){
 			cout << n.count << endl;
 		}
 	}
-	
+//оператор присваивания полетел лол	
+	void build_node_bin (){
+		node* root = node(0);
+		while (count_symb.size() > 1){
+			node* root = node(count_symb[0].count + count_symb[1].count);
+			count_symb.push_back(root);
+			if (count_symb[0].count > count_symb[1].count){
+				(*count_symb[count_symb.size()]).left_1 = count_symb[1];
+				(*count_symb[count_symb.size()]).right_0 = count_symb[0];
+			}
+			else{
+				(*count_symb[count_symb.size()]).right_0 = count_symb[1];
+				(*count_symb[count_symb.size()]).left_1 = count_symb[0];
+			}
+			auto iter = count_symb.cbegin();
+			count_symb.erase(iter);
+			count_symb.erase(iter + 1 );
+		}	
+	} 
 };
 
 
@@ -115,7 +133,11 @@ int main(){
 	main_arch.print_map_count();
 
 	main_arch.add_map_to_sort_Node();
-	main_arch.print_sort_Node();
+	main_arch.print_sort_node();
+
+	main_arch.build_node_bin();
+
+	cout << main_arch.count_symb[0].count;
 
 
 
